36c0d2d7569514d379f01d5105ea81543ba6dfa7
ivanMC-A <iamaldonado@utexas.edu>
2025-11-17 16:00:04 -0600

    Added definitions and name of functions to evaluate the adiabatic invariant.

--- src/AdiabaticInvariant.jl
+++ src/AdiabaticInvariant.jl
@@ -1,5 +1,72 @@
 module AdiabaticInvariant
+export AdiabaticInvariant, SlabJ, evaluate, deval
 
-# Write your package code here.
+using LinearAlgebra
+
+# -----------------------------------------------
+# Definitions
+# -----------------------------------------------
+
+"""
+    abstract type AdiabaticInvariant end
+
+        Parent type for all adiabatic invariant representations.
+
+"""
+
+abstract type AdiabaticInvariant end
+
+"""
+
+    struct SlabJ <: AdiabaticInvariant
+
+        Spectral representations (coefficients) of an adiabatic invariant.
+        a: Array containing Spectral coefficients.
+
+"""
+
+struct SlabJ <: AdiabaticInvariant
+
+    a::AbstractArray
 
 end
+
+# -----------------------------------------------
+# Methods
+# -----------------------------------------------
+
+function slabJ(N1,N2,N3)
+
+    a = zeros(N1,N2,N3)
+    
+    return SlabJ(a)
+end
+
+
+"""
+    evaluate(J::SlabJ,x)
+
+        Evaluate adiabatic invariant 'J' at a point 'x'.
+
+"""
+
+function evaluate(J::SlabJ,x)
+    # TODO: code spcetral reconstruction
+    error("evaluate(::$(typeof(J)), ::$(typeof(x))) not implemented")
+    
+end
+
+"""
+function deval(J::SlabJ, x)
+    Evaluate adiavatic invariant 'J' derivatives at a point 'x'.
+
+"""
+
+function deval(J::SlabJ,x)
+    # TODO: code derivatives w.r.t. X, Y, and r for J.
+    # TODO: code spcetral reconstruction for derivatives.
+    error("deval(::$(typeof(J)), ::$(typeof(x))) not implemented")
+
+end
+
+end # module
