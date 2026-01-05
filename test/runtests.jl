f18c695c15d6825fea0a997ee85bb9c3dbd0d248
ivanMC-A <iamaldonado@utexas.edu>
2026-01-05 10:56:26 -0600

    Deleted test code. Hopefully I won't get more errors

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,14 +2,5 @@ using AdiabaticInvariant
 using Test
 
 @testset "AdiabaticInvariant.jl" begin
-    # Write your tests here.
-    # I'm going to test reconstructing several functions for my fourier reconstruction func.
-
-    ## Test for f(x) = sin(x) ##
-
-    # coefficients
-    # Number of nodes I'm going to work with
-    N = 10 
-
-    @test a = zeros(N)
+    # Write your tests here
 end
