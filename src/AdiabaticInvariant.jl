7d1b1b9b7145cb47b3ee9dc987a5de9a8a3cbf8d
ivanMC-A <iamaldonado@utexas.edu>
2026-01-05 10:42:44 -0600

    Added calculation of Chebyshev Matrix and Chebyshev Coefficients

--- src/AdiabaticInvariant.jl
+++ src/AdiabaticInvariant.jl
@@ -142,4 +142,156 @@ function deval(J::SlabJ,x)
 
 end
 
-end # moduleS
+###### Chebyshev stuff
+
+"""
+    evalChev(coef::AbstractArray, x::Number)
+This function evaluates Chebyshev coefficients at a point x∈[-1,1]
+"""
+
+function evalChev(coef::AbstractArray, x::Number)
+    n_dims = ndims(coef)
+
+    if n_dims > 1
+        return error("coef::AbstractArray dimention is bigger than 1. coef must be a 1D array")
+    end
+
+    cs = 0
+    θ = acos(x)
+    for i in range(start = 0, stop = N-1)
+        cs += coef[i + 1]*cos(i*θ)
+    end
+    return cs
+end
+
+"""
+    evalChev(coef::AbstractArray, x::Number, D::AbstractArray)
+
+Evaluate the Chebyshev series described by `coef` at the physical grid point `x` that lies
+in the domain `D`. The point is affinely mapped into [-1, 1] before evaluation. `coef` must
+be a 1D array ordered as T0, T1, …, T_{N-1}; returns the scalar value of the series at `x`.
+"""
+function evalChev(coef::AbstractArray, x::Number, D::AbstractArray)
+    n_dims = ndims(coef)
+
+    if n_dims > 1
+        return error("coef::AbstractArray dimention is bigger than 1. coef must be a 1D array")
+    end
+
+    cs = 0
+    xx = grid2chev(D,x)
+    θ = acos(xx)
+    for i in range(start = 0, stop = N-1)
+        cs += coef[i + 1]*cos(i*θ)
+    end
+    return cs
+
+end
+
+"""
+    chp1st(N:Number)
+This function generates N Chebyshev points of the first kind
+"""
+
+function chept1st(N::Number)
+    x = zeros(N)
+    
+    for k in range(start = 0, stop = N-1)
+        x[k+1] = cos((k + 0.5)*π/N)
+    end
+    return x
+end
+
+"""
+    chp2nd(N:Number)
+This function generates N+1 Chebyshev points of the second kind
+"""
+
+function chept2nd(N::Number)
+    x = zeros(N+1)
+
+    for i in range(0, stop = N)
+        x[i + 1] = cos(i*π/N)
+    end
+    return x
+end
+
+"""
+    function chebpt(N::Number, k::String)
+
+Given a number 'N' and a kind 'k', this function calculates the N or N+1 Chebyshev points 
+of the first kind k = 'F' or second kind k = 'S'. 
+"""
+
+function chept(N::Number, k::String)
+
+    if k == "F"
+        x = chept1st(N)
+        return x
+    elseif k == "S"
+         x = chept2nd(N)
+        return x
+    else
+        error("'k' states the kind of chebyshev points you want. k = 'F' is for first kind and k = 'S' is for second kind")
+    end
+
+end
+
+"""
+    function chebA(N::Number)
+
+Construct the Chebyshev matrix of the first kind evaluated at Chebyshev points.
+
+"""
+
+function chebA(N::Number)
+    A = zeros(Float64,N,N)
+    xN = chept1st(N)
+    for i in 1:N
+        θn = acos(xN[i])
+        for j in 0:(N-1)
+            A[i,j+1] = cos((j) * θn)
+        end
+    end
+    return A
+end
+
+"""
+    function getChebCoef(N::Number, FN::AbstractArray)
+
+Compute Chebyshev polynomial coefficients from function evaluated at Chebyshev points.
+
+"""
+
+function getChebCoef(N::Number, FN::AbstractArray)
+    A = chebA(N)
+    At = A'
+    W = At * A
+    b = At * FN
+    x = W \ b
+    return x
+end
+
+"""
+    function chev2grid(D::AbstractArray,y::Number)
+Given a chebyshev point y and a domain D, this function maps the point y back to a number x ∈ D
+"""
+function chev2grid(D::AbstractArray,y::Number)
+    a = D[1]
+    b = D[end]
+    x = 0.5*((a-b)*y + (a+b))
+    return x
+end
+
+"""
+    function grid2chev(D::AbstractArray,y::Number)
+Given a point x ∈ D and a Chebyshev domain [-1,1], this function maps the point x back to a y x ∈ [-1,1]
+"""
+function grid2chev(D::AbstractArray,x::Number)
+    a = D[1]
+    b = D[end]
+    y = 2/(b-a)*(x-a) - 1
+    return y
+end
+
+end # module
